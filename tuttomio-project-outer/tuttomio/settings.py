a698ef020abdb15ff63fe33412c4761678ed7015
M Kunde <mikolaj.kundegorski@gmail.com>
2022-05-13 13:09:22 +0600

    basic template override

--- tuttomio-project-outer/tuttomio/settings.py
+++ tuttomio-project-outer/tuttomio/settings.py
@@ -52,11 +52,10 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'tuttomio.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
